672c2fdcabd7566ca72ccdfebab788b104999030
Brubi25 <148341586+Brubi25@users.noreply.github.com>
2024-11-30 12:47:22 +0100

    Update AutoRepoWindows.bat

--- AutoRepoWindows.bat
+++ AutoRepoWindows.bat
@@ -6,16 +6,13 @@ set /p server="Server > "
 set "port=22"
 set /p port="Port (default: 22) > " 
 
-cd ..
-attrib +h AutoRepo
-
 if not exist .git git init
 git add . || goto nogit
 git commit -m "first commit"
 git remote set-url "origin" ssh://%username%@%server%:%port%%dir% || git remote add "origin" ssh://%username%@%server%:%port%%dir% || goto badServerPath
 ssh -p %port% %username%@%server% "mkdir %dir% && cd %dir% && git init && git config receive.denyCurrentBranch ignore && printf '#!/bin/sh\ngit --git-dir=. --work-tree=.. checkout -f' > .git/hooks/post-receive && chmod +x .git/hooks/post-receive" || goto badServerPath
 git push --set-upstream origin master
-rmdir /q /s AutoRepo
+del AutoRepoWindows.bat
 goto end
 
 :nogit
@@ -27,6 +24,4 @@ goto end
     goto error
 :error
     rmdir /q /s .git
-    attrib -h .AutoRepoW
-    cd AutoRepo
 :end
