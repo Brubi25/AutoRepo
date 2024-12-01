75fec260c485165c932efe087e2e05d6a73f04e7
Brubi25 <148341586+Brubi25@users.noreply.github.com>
2024-12-01 17:24:43 +0100

    changes

--- AutoRepoWindows.bat
+++ AutoRepoWindows.bat
@@ -10,11 +10,16 @@ set /p port="Port (default: 22) > "
 
 cd %localPath% || goto badLocalPath
 
-if not exist .git git init
+if not exist .git (
+    git init 
+    echo "# git_set_upstream" >> README.md
+) 
 git add . || goto nogit
 git commit -m "first commit"
+
 git remote set-url "origin" ssh://%username%@%server%:%port%%dir% || git remote add "origin" ssh://%username%@%server%:%port%%dir% || goto badServerPath
-ssh -p %port% %username%@%server% "mkdir %dir% && cd %dir% && git init && git config receive.denyCurrentBranch ignore && printf '#!/bin/sh\ngit --git-dir=. --work-tree=.. checkout -f' > .git/hooks/post-receive && chmod +x .git/hooks/post-receive" || goto badServerPath
+ssh -p %port% %username%@%server% "mkdir %dir%; cd %dir% && git init && git config receive.denyCurrentBranch ignore && printf '#!/bin/sh\ngit --git-dir=. --work-tree=.. checkout -f' > .git/hooks/post-receive && chmod +x .git/hooks/post-receive" || goto badServerPath
+
 git push --set-upstream origin master
 goto end
 
@@ -30,4 +35,5 @@ goto end
     goto end
 :error
     rmdir /q /s .git
+    del README.md
 :end
